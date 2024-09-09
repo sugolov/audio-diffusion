2433e940af9deb983aaf64965fe85832d130e769
rayankermanshahani <rayan.k@gmx.com>
2024-09-09 13:09:56 -0400

    include optional arg for segment length

--- data/process_sc.sh
+++ data/process_sc.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-./src/preprocessor.py ./training_data/raw_audio/soundcloud_audio/ ./training_data/processed_audio/soundcloud_processed/
+SEGMENT_LEN=0.5
+# check if argument is provided
+if [ $# -eq 1 ]; then
+    SEGMENT_LEN=$1
+fi
+
+./src/preprocessor.py ./training_data/raw_audio/soundcloud_audio/ ./training_data/processed_audio/soundcloud_processed/ --segment_length $SEGMENT_LEN
--- data/process_yt.sh
+++ data/process_yt.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-./src/preprocessor.py ./training_data/raw_audio/youtube_audio/ ./training_data/processed_audio/youtube_processed/
+SEGMENT_LEN=0.5
+# check if argument is provided
+if [ $# -eq 1 ]; then
+    SEGMENT_LEN=$1
+fi
+
+./src/preprocessor.py ./training_data/raw_audio/youtube_audio/ ./training_data/processed_audio/youtube_processed/ --segment_length $SEGMENT_LEN
